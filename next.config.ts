bccf0ef4a859e789769518203722caeff56c29bb
EternZiv <abdullahrashid13674533@gmail.com>
2026-06-25 22:03:32 +0500

    Add GitHub Pages deployment

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === "production";
+
 const nextConfig: NextConfig = {
+  output: "export",
+  trailingSlash: true,
+
+  basePath: isProd ? "/power2go" : "",
+  assetPrefix: isProd ? "/power2go/" : "",
+
   images: {
+    unoptimized: true,
     remotePatterns: [
       {
         protocol: "https",
